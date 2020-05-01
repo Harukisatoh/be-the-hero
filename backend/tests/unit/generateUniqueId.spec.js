d64ba1f56e37ba3ef456d2bc270eaf6e4775653b
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2020-05-01 18:40:37 -0300

    fix: generateUniqueId tests fixed

--- backend/tests/unit/generateUniqueId.spec.js
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -3,8 +3,8 @@ const generateUniqueId = require('../../src/utils/generateUniqueId');
 // Generate ID tests
 describe('Generate Unique ID', () => {
   // Tests if the function is able to generate an unique ID with 4 bytes
-  it('should generate an ID with 4 bytes', () => {
-    const id = generateUniqueId();
+  it('should generate an ID with 4 bytes', async () => {
+    const id = await generateUniqueId();
 
     // Tests
     expect(typeof 'id').toBe('string'); // Checks if the ID value is string type
